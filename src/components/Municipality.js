e7cd860de5004b034e192fdf4d985d60ad85acef
scasasr <CORREO@example.com>
2023-02-28 17:26:11 -0500

    init CRUD  schema

---
+++ src/components/Municipality.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Municipality = () => {
+    return (  <>
+        <h1>Municipios</h1>
+    </>);
+}
+ 
+export default Municipality;
---
+++ src/components/ResponsiblePerson.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ResponsiblePerson = () => {
+    return (  <>
+        <h1>Cabeza de familias</h1>
+    </>);
+}
+ 
+export default ResponsiblePerson;
