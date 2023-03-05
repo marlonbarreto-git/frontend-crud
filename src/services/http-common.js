c4ac1196d02c06f409a8bdaa48db429b780697e2
scasasr <CORREO@example.com>
2023-03-04 23:55:20 -0500

    CRUD responsible person

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "",
+    baseURL: "http://localhost:8080/v1",
     headers: {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
